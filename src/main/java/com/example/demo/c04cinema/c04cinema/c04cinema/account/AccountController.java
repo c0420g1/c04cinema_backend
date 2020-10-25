d92906c23fe10deb1024106e9eb0a55d34de5d07
huyduong155 <nhathuy.155@gmail.com>
2020-10-25 21:05:37 +0700

    huy-login-backend

--- src/main/java/com/example/demo/c04cinema/c04cinema/c04cinema/account/AccountController.java
+++ src/main/java/com/example/demo/c04cinema/c04cinema/c04cinema/account/AccountController.java
@@ -1,7 +1,20 @@
 package com.example.demo.c04cinema.c04cinema.c04cinema.account;
 
 import com.example.demo.c04cinema.c04cinema.c04cinema.account.generated.GeneratedAccountController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-public class AccountController extends GeneratedAccountController {}
+public class AccountController extends GeneratedAccountController {
+    @Autowired
+    private AccountManager accountManager;
+
+    @GetMapping("/accountIdFirst")
+    public Account getAllAccount(){
+        return accountManager.stream().sorted(Account.ID.reversed()).findFirst().get();
+    }
+}
