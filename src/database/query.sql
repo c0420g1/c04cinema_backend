b4f0733a314722bc43b861ab2fcb418d547c05d2
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-10-30 10:14:27 +0700

    update-db

---
+++ src/database/query.sql
@@ -0,0 +1,8 @@
+SET SQL_SAFE_UPDATES = 0;
+update booking_ticket set booking_date = "2020-10-10 07:00";
+update booking_ticket set status = "0";
+update customer set `code` = "KH-1234";
+update customer set image_url = "../../../assets/images/customer/11333831.jpg";
+update staff set `code` = "KH-1234";
+update staff set image_url = "../../../assets/images/customer/11333831.jpg";
+update movie set trailer_url = "https://www.youtube.com/watch?v=dfnCAmr569k";
