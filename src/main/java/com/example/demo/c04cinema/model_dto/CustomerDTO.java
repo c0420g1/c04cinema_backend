4be631db235549555ff15db573657f8cfb93ef51
thinhhv1993 <64394302+thinhhv1993@users.noreply.github.com>
2020-11-09 09:51:31 +0700

    a

--- src/main/java/com/example/demo/c04cinema/model_dto/CustomerDTO.java
+++ src/main/java/com/example/demo/c04cinema/model_dto/CustomerDTO.java
@@ -8,18 +8,30 @@ import com.example.demo.c04cinema.c04cinema.c04cinema.show.Show;
 import java.time.LocalDateTime;
 
 public class CustomerDTO {
+    private final int idAcc;
     private final String movieName;
     private final LocalDateTime dayPut;
     private final Double totalMoney;
     private final Byte status;
     private final Byte canCel;
+    private final int idTicker;
 
     public CustomerDTO(Account account, BookingTicket bookingTicket, Show show, Movie movie){
+        this.idAcc = account.getId();
         this.movieName= movie.getName().get();
         this.dayPut= bookingTicket.getBookingDate().get();
         this.totalMoney= bookingTicket.getPrice().getAsDouble();
         this.status= bookingTicket.getStatus().get();
         this.canCel= bookingTicket.getIscancel().get();
+        this.idTicker = bookingTicket.getId();
+    }
+
+    public int getIdAcc() {
+        return idAcc;
+    }
+
+    public int getIdTicker() {
+        return idTicker;
     }
 
     public String getMovieName() {
